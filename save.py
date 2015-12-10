55d0e80f545d1e962d7f1c71930a20c4a388a7c2
jajaio <jackm2580@gmail.com>
2015-12-10 09:05:16 -0500

    save

--- save.py
+++ save.py
@@ -1,13 +1,16 @@
 author="jajaio"
 import json
-impp
+import classes as cl
 #Work in progress. May not make it into final release.
 def save():
 	with open('player.json', 'w') as pfile:
 		pfile.write(json.dumps({
-        " 
+            "hp":cl.Player.hp,
+            "agi":cl.Player.agi,
+            "deff":cl.Player.deff,
+            "att":cl.Player.att,
+            "mp":cl.Player.mp
+            }))
         
-
-
-
-        }))
+if __name__=='__main__':
+    save()

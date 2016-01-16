1ce792e96e0f4a8bb27ca5209db717c3101ca0ae
jajaio <jackm2580@gmail.com>
2016-01-16 17:02:04 -0500

    save

--- field.py
+++ field.py
@@ -2,14 +2,36 @@ author="jajaio"
 import battleengine as b
 import random
 import classes as cl
+import colors as c
+
 def random_monster():
     monsters=[cl.Slime, cl.Bandit, cl.Zombie, cl.Skeleton, cl.Goblin]
     monster=random.choice(monsters)
     cl.Foe=monster
-    print(cl.Foe.mname)
 
 def field():
-    print("You arive in a grassy field.")
+    print("You are in a grassy field.")
+    f=input("Do you want to look for monsters? Or go back to the town? (1), (2)")
+    if f=="1":
+        print("You decide to look around.")
+        t.sleep(1.5)
+        print(c.clear)
+        print(".")
+        print(c.clear)
+        print("..")
+        print(c.clear)
+        print("...")
+        print(c.clear)
+        random_monster()
+        print("You found a random "+cl.Foe.mname+"!")
+        t.sleep(1)
+        b.fight()
+        field()
+    elif f=="2":
+        pass
+    else:
+        pass
 
 if __name__=='__main__':
     random_monster()
+    print(cl.Foe.mname)

e23112762f29329041c24fd926250e14d9bce061
Jackson W. Martin <jackm2580@gmail.com>
2016-01-28 17:45:12 -0500

    save

--- battleengine.py
+++ battleengine.py
@@ -21,6 +21,7 @@ def foeattack():
     print(c.yellow+"Your foe strikes you!")
     t.sleep(1)
     player.hp -= monster.att
+    player.hp += player.deff
 
 def foeheal():
     global player, monster
@@ -28,6 +29,7 @@ def foeheal():
         print(c.yellow+"Your foe tried to heal, but attacked instead!")
         t.sleep(1)
         player.hp -= monster.att
+        player.hp += player.deff
     else:
         print(c.yellow+"Your foe Heals")
         t.sleep(1)
